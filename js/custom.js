17676b69798ceef4d6249ba7b8d065934d711e06
zackwong <hzzzoo@126.com>
2018-10-24 12:04:50 +0800

    up

--- js/custom.js
+++ js/custom.js
@@ -28,18 +28,4 @@ jQuery(document).ready(function(){
 
   });
 
-//============================== Date-picker =========================
-  $('.datepicker').datepicker({
-    startDate: 'dateToday',
-    autoclose: true
-  });
-
-//============================== COUNTER-UP =========================
-  $(document).ready(function ($) {
-    $('.counter').counterUp({
-      delay: 10,
-      time: 2000
-    });
-  });
-
   });
