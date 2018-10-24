845a4536e5098bc8cffa1cc7e42348ff234d3af4
zackwong <hzzzoo@126.com>
2018-10-24 12:01:46 +0800

    up

--- js/custom.js
+++ js/custom.js
@@ -175,36 +175,3 @@ jQuery(document).ready(function(){
 
 
   });
-
-  //============================== BRAND SLIDER =========================
-  $('.brandSlider').slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    centerMode: true,
-    centerPadding: '0px',
-    responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true
-      }
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
-
-  });
